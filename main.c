51c8b903726c703c60870c01797ace43c07d20e0
Riccardo Casatta <riccardo@casatta.it>
2019-02-19 10:37:17 +0100

    aa

--- main.c
+++ main.c
@@ -5,10 +5,11 @@
 #define ARCS 19
 
 struct Arc {
-   double start_radius;  // 0..1 if 1 eq to SIZE
-   double end_radius;
-   double min_radiant;
-   double max_radiant;
+   double internal_radius;  // 0..1 if 1 eq to SIZE
+   double external_radius;
+   double start_radiant;
+   double end_radiant;
+   double inc_radiant;
 };
 
 void print_matrix(int m[SIZE][SIZE]) {
@@ -32,14 +33,14 @@ void draw_circle(int m[SIZE][SIZE], struct Arc *c[ARCS]) {
             d = (int) sqrt( (double) i*i+j*j );
             theta = atan2((double)i, (double)j);
             for (int k=0;k<ARCS;k++) {
-                int a = (int) (c[k]->start_radius * SIZE);
-                int b = (int) (c[k]->end_radius * SIZE);
+                int a = (int) (c[k]->internal_radius * SIZE);
+                int b = (int) (c[k]->external_radius * SIZE);
         
-                //printf("k:%d i:%d j:%d d:%d min:%f theta:%f max:%f\n",k,i,j,d,c[k]->min_radiant,theta,c[k]->max_radiant);
+                //printf("k:%d i:%d j:%d d:%d min:%f theta:%f max:%f\n",k,i,j,d,c[k]->start_radiant,theta,c[k]->end_radiant);
                 if ( a<d && b>d && 
-                      ( (c[k]->min_radiant < theta && c[k]->max_radiant > theta) ||
-                        (c[k]->min_radiant < (theta - 2 * PI) && c[k]->max_radiant > (theta - 2 * PI) ) ||
-                        (c[k]->min_radiant < (theta + 2 * PI) && c[k]->max_radiant > (theta + 2 * PI) ) )
+                      ( (c[k]->start_radiant < theta && c[k]->end_radiant > theta) ||
+                        (c[k]->start_radiant < (theta - 2 * PI) && c[k]->end_radiant > (theta - 2 * PI) ) ||
+                        (c[k]->start_radiant < (theta + 2 * PI) && c[k]->end_radiant > (theta + 2 * PI) ) )
                    )  {
                     m[i+half][j+half]=1;
                 }
@@ -56,58 +57,46 @@ int reset(int m[SIZE][SIZE]) {
     } 
 } 
 
-int inc_rad(struct Arc *c, double inc) {
+int inc_rad(struct Arc *c) {
     
-    c->min_radiant+=inc;
-    c->max_radiant+=inc;
-    if (c->max_radiant > PI && c->min_radiant > PI) {
-        c->max_radiant-= 2*PI;
-        c->min_radiant-= 2*PI;
+    c->start_radiant+=c->inc_radiant;
+    c->end_radiant+=c->inc_radiant;
+    if (c->end_radiant > PI && c->start_radiant > PI) {
+        c->end_radiant-= 2*PI;
+        c->start_radiant-= 2*PI;
     }
-    if (c->max_radiant < -PI && c->min_radiant < -PI) {
-        c->max_radiant+= 2*PI;
-        c->min_radiant+= 2*PI;
+    if (c->end_radiant < 0 && c->start_radiant < 0) {
+        c->end_radiant+= 2*PI;
+        c->start_radiant+= 2*PI;
     }
 }
 
 void print_arc(int i, struct Arc *a) {
     
-    printf("%d start:%.2f end:%.2f min:%.3f max:%.3f\n",i, a->start_radius, a->end_radius, a->min_radiant, a->max_radiant);
+    printf("%d start:%.2f end:%.2f min:%.3f max:%.3f\n",i, a->internal_radius, a->external_radius, a->start_radiant, a->end_radiant);
 }
 
 int main()
 {
     int m[SIZE][SIZE];
-    /*struct Arc c2;
-    c2.start_radius=0.35;
-    c2.end_radius=0.45;
-    c2.min_radiant=0.0;
-    c2.max_radiant=PI/4;
-    
-    struct Arc c1;
-    c1.start_radius=0.15;
-    c1.end_radius=0.25;
-    c1.min_radiant=0.0;
-    c1.max_radiant=PI/4;
-    */
-    //struct Arc *c[ARCS] = {&c1,&c2};
     struct Arc *c[ARCS];
     double carc = 2 * PI / ARCS;
     double space = carc / 6;
     printf("carc:%f space:%f\n",carc, space);
     for (int i=0;i<ARCS;i++) {
         struct Arc *t = (struct Arc *) malloc(sizeof(struct Arc));
-        t->start_radius=0.39;
-        t->end_radius=0.48;
-        t->min_radiant=i * carc;
-        t->max_radiant=(i+1) * carc - space;
+        t->internal_radius=0.39;
+        t->external_radius=0.48;
+        t->start_radiant=i * carc;
+        t->end_radiant=(i+1) * carc - space;
+	t->inc_radiant=0.05;
         c[i]=t;
     }
 
     while(1) {
         reset(m);
         for (int i=0;i<ARCS;i++) {
-            inc_rad(c[i],0.05);
+            inc_rad(c[i]);
         }
         //inc_rad(c[1],-0.05);
         draw_circle(m,c);
